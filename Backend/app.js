d95c6071d4b209eef6261c849a1a5ddd9b7a6fd5
chevvuri kishore <Chevvuri Kishore>
2026-08-30 12:55:17 +0530

    authenication completed-4

--- Backend/app.js
+++ Backend/app.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://vidyasetufrontend.netlify.app/",
     credentials: true,
   })
 );
